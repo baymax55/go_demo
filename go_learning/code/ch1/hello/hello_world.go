90e5c082e37ee07725539e33083a69e21b2637c0
baymax <baymax56@dingtalk.com>
2022-01-06 09:42:37 +0800

    add go_learning

---
+++ go_learning/code/ch1/hello/hello_world.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//if len(os.Args) > 1 {
+	fmt.Println("Hello World")
+	//fmt.Println("Hello World", os.Args[1])
+	fmt.Print("1111111111111111")
+	//}
+}
