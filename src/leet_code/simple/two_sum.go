96485e4ec48db936203fb04ae33ea8b49edadd4f
baymax <xingbai56@gmail.com>
2022-01-10 17:17:24 +0800

    [leet-code] add twoSum

---
+++ src/leet_code/simple/two_sum.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/* 创建切片 */
+	arr := []int{1, 2, 3}
+	x := twoSum(arr, 3)
+	printSlice(x)
+}
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		another := target - nums[i]
+		if _, ok := m[another]; ok {
+			return []int{m[another], i}
+		}
+		m[nums[i]] = i
+	}
+	return nil
+}
+func printSlice(x []int) {
+	fmt.Printf("len=%d cap=%d slice=%v\n", len(x), cap(x), x)
+}
