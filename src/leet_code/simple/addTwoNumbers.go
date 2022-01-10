d322f6ced9ee2255c0c4f0a4606c5342ec49cf31
baymax <xingbai56@gmail.com>
2022-01-10 18:11:13 +0800

    [leet-code] add addTwoNumbers

---
+++ src/leet_code/simple/addTwoNumbers.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+	l1 := ListNode{Val: 1}
+	l2 := ListNode{Val: 2}
+	l := addTwoNumbers(&l1, &l2)
+	fmt.Printf("%v", l.Val)
+}
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	head := &ListNode{Val: 0}
+	n1, n2, carry, current := 0, 0, 0, head
+	for l1 != nil || l2 != nil || carry != 0 {
+		if l1 == nil {
+			n1 = 0
+		} else {
+			n1 = l1.Val
+			l1 = l1.Next
+		}
+		if l2 == nil {
+			n2 = 0
+		} else {
+			n2 = l2.Val
+			l2 = l2.Next
+		}
+		current.Next = &ListNode{Val: (n1 + n2 + carry) % 10}
+		current = current.Next
+		carry = (n1 + n2 + carry) / 10
+	}
+	return head.Next
+}
