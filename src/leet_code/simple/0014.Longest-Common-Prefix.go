907d453f6d12532acb1f3a7ee0f78e12daed7d9d
baymax <xingbai56@gmail.com>
2022-01-24 14:09:16 +0800

    [leet-code] add 0014.Longest-Common-Prefix.go

---
+++ src/leet_code/simple/0014.Longest-Common-Prefix.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+	//str := []string{"aflower", "sflow", "dflight"}
+	str := []string{"flower", "flow", "flight"}
+
+	println(longestCommonPrefix(str))
+}
+
+func longestCommonPrefix(strs []string) string {
+	prefix := strs[0]
+
+	for i := 1; i < len(strs); i++ {
+		for j := 0; j < len(prefix); j++ {
+			if len(strs[i]) <= j || strs[i][j] != prefix[j] {
+				prefix = prefix[0:j]
+				break
+			}
+		}
+	}
+
+	return prefix
+}
